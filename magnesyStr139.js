a7193cca97cb0c89d1d4d5e690a5fce73ff9b3e9
loduva <loduva@gmail.com>
2016-11-16 11:33:45 +0100

    Add files via upload

---
+++ magnesyStr139.js
@@ -0,0 +1,8 @@
+
+var products = ["Choco", "ice Mint", "Cake Batter", "Bubblegum"];
+var hasBubbleGum = [false, false, false, true];
+for (var i = 0; i < hasBubbleGum.length; i++)
+{
+	if (hasBubbleGum[i])
+	{ console.log(products[i] + " contains bubble gum"); }
+}
